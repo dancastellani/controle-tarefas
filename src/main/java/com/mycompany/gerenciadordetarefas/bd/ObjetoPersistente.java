b842d4b6d9b1f9a0fa313ce778e9921bdbb1cb33
Daniel Castellani <dancastellani@gmail.com>
2013-06-19 21:25:58 -0300

    Criado o Banco de Dados

---
+++ src/main/java/com/mycompany/gerenciadordetarefas/bd/ObjetoPersistente.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gerenciadordetarefas.bd;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author DanCastellani
+ */
+public abstract class ObjetoPersistente implements Serializable {
+}

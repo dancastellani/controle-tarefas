3794d61268740cfcb67208f4f1c5921940994083
Daniel Castellani <dancastellani@gmail.com>
2013-06-21 16:33:53 -0300

    Criacao da classe Tarefa

---
+++ src/main/java/com/mycompany/gerenciadordetarefas/modelo/Tarefa.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gerenciadordetarefas.modelo;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Tarefa {
+
+    private String descricao;
+    private Calendar dataDeCriacao;
+    private Calendar dataDeVencimento;
+    private Calendar dataDeFechamento;
+}
